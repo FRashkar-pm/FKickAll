434bcf524a22bf52f454145e1c516df2f089f636
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2023-05-12 22:14:55 +0700

    new FKickAll

---
+++ src/FRashkar/KickAllPlayers/Commands/FKickCommand.php
@@ -0,0 +1,95 @@
+<?php
+    
+/**
+*  ______ _____           _     _                                     
+* |  ____|  __ \         | |   | |                                    
+* | |__  | |__) |__ _ ___| |__ | | ____ _ _ __ ______ _ __  _ __ ___  
+* |  __| |  _  // _` / __| '_ \| |/ / _` | '__|______| '_ \| '_ ` _ \ 
+* | |    | | \ \ (_| \__ \ | | |   < (_| | |         | |_) | | | | | |
+* |_|    |_|  \_\__,_|___/_| |_|_|\_\__,_|_|         | .__/|_| |_| |_|
+*                                                    | |              
+*                                                    |_|              
+* The author of this plugin is FRashkar-pm
+* Github: https://github.com/FRashkar-pm
+* Discord: FireRashkar#1519
+*/
+
+namespace FRashkar\KickAllPlayers\Commands;
+
+use FRashkar\KickAllPlayers\Loader;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\player\Player;
+use pocketmine\plugin\{Plugin, PluginOwned};
+use pocketmine\utils\TextFormat;
+
+class FKickCommand extends Command implements PluginOwned
+{
+    /** var @Loader */
+    private Loader $loader;
+    
+    public function __construct(Loader $loader)
+    {
+        $this->loader = $loader;
+        parent::__construct("kickall", "Kick All Commands", "/kickall <reason>", ["fk"]);
+    }
+    
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {   
+        if(!$this->testPermission($sender))
+        {
+            return;
+        }
+        
+        if(!$sender instanceof Player)
+        {
+            if($args == "")
+            {
+                if($sender->getServer()->getOnlinePlayers == null)
+                {
+                    $sender->getServer()->getLogger()->info("[FKickAll] => There are no players to kick!");
+                }else{
+                    foreach($sender->getServer()->getOnlinePlayers() as $players)
+                    {
+                        $players->kick("", false);
+                    }
+                    $sender->getServer()->getLogger()->info("[FKickAll] => All players have been kicked!");
+                }
+            }else{
+                if($sender->getServer()->getOnlinePlayers() == null)
+                {
+                    $sender->getServer()->getLogger()->info("[FKickAll] => There are no players to kick!");
+                }else{
+                    foreach($sender->getServer()->getOnlinePlayers() as $players)
+                    {
+                        $players->kick(implode(" ", $args), false);
+                    }
+                    $sender->getServer()->getLogger()->info("[FKickAll] => All players have been kicked!");
+                }
+            }
+        }
+        elseif($sender->hasPermission("kickall.use"))
+        {
+            if($args == "")
+            {
+                foreach($sender->getServer()->getOnlinePlayers() as $players)
+                {
+                    $players->kick("", true);
+                }
+            }else{
+                foreach($sender->getServer()->getOnlinePlayers() as $players)
+                {
+                    $players->kick(implode(" ", $args), true);
+                }
+            }
+            $name = $sender->getName();
+            $sender->getServer()->getLogger()->info("[FKickAll] => All players have been kicked by $name !");
+        }else{
+            $sender->sendMessage(TextFormat::RED . "Sorry you don't have permission to use this commands!");
+        }
+    }
+    
+    public function getOwningPlugin(): Plugin
+    {
+        return Loader::getInstance();
+    }
+}
---
+++ src/FRashkar/KickAllPlayers/Loader.php
@@ -0,0 +1,32 @@
+<?php
+    
+/**
+*  ______ _____           _     _                                     
+* |  ____|  __ \         | |   | |                                    
+* | |__  | |__) |__ _ ___| |__ | | ____ _ _ __ ______ _ __  _ __ ___  
+* |  __| |  _  // _` / __| '_ \| |/ / _` | '__|______| '_ \| '_ ` _ \ 
+* | |    | | \ \ (_| \__ \ | | |   < (_| | |         | |_) | | | | | |
+* |_|    |_|  \_\__,_|___/_| |_|_|\_\__,_|_|         | .__/|_| |_| |_|
+*                                                    | |              
+*                                                    |_|              
+* The author of this plugin is FRashkar-pm
+* https://github.com/FRashkar-pm
+* Discord: FireRashkar#1519
+*/
+
+namespace FRashkar\KickAllPlayers;
+
+use FRashkar\KickAllPlayers\Commands\FKickCommand;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class Loader extends PluginBase
+{
+    use SingletonTrait;
+    
+    public function onEnable(): void 
+    {
+        self::setInstance($this);
+        $this->getServer()->getCommandMap()->register("kickall", new FKickCommand($this));
+    }
+}
